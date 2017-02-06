57ccbb963769043b32c2061f6257340fec83249c
Ayder Abdurafieiev <aabdurafieiev@ayder-mbp.local>
2017-02-06 13:09:31 -0600

    fix column width for horizontal scroll

--- app/modules/main/factories/TableDataStorageFactory.js
+++ app/modules/main/factories/TableDataStorageFactory.js
@@ -92,9 +92,10 @@
                 });
             });
             var _tableWidth = 0;
-            header.style = header.style || {};
+
 
             _header.forEach(function (header, index) {
+                header.style = header.style || {};
                 if (!header.style['width'] || header.style['width'] < _maxWidth[header.id]) {
                     header.style['width'] = Math.round(_maxWidth[header.id]);
                 }

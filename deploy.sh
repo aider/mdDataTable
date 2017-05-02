8385acaae07e210b93fcdc03a17274cefe26e978
Ayder Abdurafieiev <aabdurafieiev@ayder-mbp.local>
2017-05-02 12:40:19 -0500

    fix calc scrollVisible

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 gulp release
-cp /Users/aabdurafieiev/_work/mdDataTable/dist/md-data-table.js /Users/aabdurafieiev/project/libs/material/src/components/table/table.js
-echo "1"
-cp /Users/aabdurafieiev/_work/mdDataTable/app/scss/main.scss /Users/aabdurafieiev/project/libs/material/src/components/table/table.scss
-echo "2"
-cd /Users/aabdurafieiev/project/libs/material/
-echo "3"
-gulp build
-cp -R /Users/aabdurafieiev/project/libs/material/dist/* /Users/aabdurafieiev/project/libs/bower-material/
-echo "4"
-cp -R /Users/aabdurafieiev/project/libs/bower-material/* /Users/aabdurafieiev/_work/onsdev/github/OnStandards.Web.dev/app/bower_components/angular-material
-cp -R /Users/aabdurafieiev/project/libs/bower-material/* /Users/aabdurafieiev/project/git/Kenekt/Web/app/bower_components/angular-material
+#cp /Users/aabdurafieiev/_work/mdDataTable/dist/md-data-table.js /Users/aabdurafieiev/project/libs/material/src/components/table/table.js
+#echo "1"
+#cp /Users/aabdurafieiev/_work/mdDataTable/app/scss/main.scss /Users/aabdurafieiev/project/libs/material/src/components/table/table.scss
+#echo "2"
+#cd /Users/aabdurafieiev/project/libs/material/
+#echo "3"
+#gulp build
+#cp -R /Users/aabdurafieiev/project/libs/material/dist/* /Users/aabdurafieiev/project/libs/bower-material/
+#echo "4"
+#cp -R /Users/aabdurafieiev/project/libs/bower-material/* /Users/aabdurafieiev/_work/onsdev/github/OnStandards.Web.dev/app/bower_components/angular-material
+#cp -R /Users/aabdurafieiev/project/libs/bower-material/* /Users/aabdurafieiev/project/git/Kenekt/Web/app/bower_components/angular-material
